5ab77521a50db821cb531f5d426b1314385fe5be
Arthur Zhang <kingdominecraft@outlook.com>
2018-05-06 20:19:37 -0400

    Email Tests

--- form-to-email.php
+++ form-to-email.php
@@ -1,4 +1,5 @@
 <?php
+require_once '/var/www/html/web1/vendor/swiftmailer/swiftmailer/lib/swift_required.php';
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
   $device = $_POST['device'];
@@ -18,21 +19,20 @@ $headers = "From: $name \r\n";
 
 $headers .= "Reply-To: $visitor_email \r\n";
 
-$smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'repairbox4@gmail.com',
-        'password' => 'Repairbox2018'
-    ));
 
-$mail = $smtp->send($to, $headers, $body);
 
-if (PEAR::isError($mail)) {
-    echo('<p>' . $mail->getMessage() . '</p>');
-} else {
-    echo('<p>Message successfully sent!</p>');
-}
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+  ->setUsername('Repairbox4@gmail.com')
+  ->setPassword('Repairbox2018');
+
+$mailer = Swift_Mailer::newInstance($transport);
+
+$message = Swift_Message::newInstance('Test Subject')
+  ->setFrom(array('Repairbox4@gmail.com' => 'Repair Box'))
+  ->setTo(array('azhangvo@gmail.com', 'Repairbox4@gmail.com'))
+  ->setBody($email_body);
+
+$result = $mailer->send($message);
 
 //mail($to,$email_subject,$email_body,$headers);
 //header("Location: ./index.html");
