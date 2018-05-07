4c6ace57648255cafbd0bb8990e52ed628da7398
Arthur Zhang <kingdominecraft@outlook.com>
2018-05-06 20:09:06 -0400

    Email Fix Attempt

--- form-to-email.php
+++ form-to-email.php
@@ -4,15 +4,13 @@
   $device = $_POST['device'];
   $request = $_POST['request'];
   $other = $_POST['other'];
-?>
-<?php
+
 	$email_from = $visitor_email;
 
 	$email_subject = "New Request from $name";
 
 	$email_body = "$name ($visitor_email) has submitted a request. Device - $device. Request - \n \n $message \n \n Other - \n \n $other"
-?>
-<?php
+
 
 $to = "repairbox4@gmail.com, azhangvo@gmail.com";
 
@@ -20,6 +18,22 @@ $headers = "From: $name \r\n";
 
 $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
-header("Location: ./index.html");
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'repairbox4@gmail.com',
+        'password' => 'Repairbox2018'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}
+
+//mail($to,$email_subject,$email_body,$headers);
+//header("Location: ./index.html");
 ?>
